2a165c0c5d3f0c5616f60efd775a941bd5b1511a
Dominik Deren <dominik.deren@live.com>
2022-08-24 12:19:58 +0200

    Fixing typos.

--- tests/test_js.py
+++ tests/test_js.py
@@ -1726,7 +1726,6 @@ class KVTest(SingleJetStreamServerTestCase):
 
         with pytest.raises(BadBucketError):
             await js.key_value(bucket="TEST3")
-<<<<<<< HEAD
 
 
 class OBJTest(SingleJetStreamServerTestCase):
